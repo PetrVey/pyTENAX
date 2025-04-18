3e5989b27a58e210473715ce68a2396671909749
Petr Vohnicky <vohnickypetr@gmail.com>
2025-04-18 22:34:34 +0200

    read the docs try

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -11,6 +11,15 @@ copyright = '2025, Petr Vohnicky'
 author = 'Petr Vohnicky'
 release = '0.0.1'
 
+import os
+import sys
+from pathlib import Path
+
+# Add the repository's src directory to sys.path
+repo_path = Path(__file__).resolve().parents[2]  # Adjust according to the structure
+src_path = repo_path / "src"
+sys.path.insert(0, str(src_path))
+
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
